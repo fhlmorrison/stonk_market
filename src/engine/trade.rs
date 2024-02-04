0c34007449195880ae147d1f988c9fbc5d80294c
Fraser Morrison <frazemor@hotmail.com>
2024-02-04 11:27:17 -0500

    removed dead files

--- src/engine/trade.rs
+++
@@ -1,7 +0,0 @@
-fn execute_trade(buy: &Order, sell: &Order, quantity: u64) {
-    println!(
-        "Trade executed: {} => {} for {} @ {}",
-        buy.id, sell.id, quantity, sell.price
-    );
-    // todo!();
-}
